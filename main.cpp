3ed16f2b1754c0dbd4b6d726624c62b146b00b6e
OlawaleA <olawaleayejuyone@gmail.com>
2017-10-03 17:41:24 -0400

    modified main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
-#include <iostream>
-
+#include "sample.h"
+using namespace exercises;
 int main()
 {
-  std:: cout << "Hello world \n";
+  Exercise ex;
+  ex.x =4; // x field of object exz
+  ex.y = "Hello"; //y field of object ex
+  ex.print();// print() mathod of object ex
+
+  std:: cout << "\n";
+  std:: cout << ex.mult()<< "\n";
   return 0;
 }
